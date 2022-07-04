d03f43a92207a2910842a23fc0e55958a0482819
Bobo~ <yanglinself@gmail.com>
2022-07-04 10:59:40 +0800

    Update eval.py

--- scripts/eval.py
+++ scripts/eval.py
@@ -100,7 +100,7 @@ def run(
         save_hybrid=False,  # save label+prediction hybrid results to *.txt
         save_conf=False,  # save confidences in --save-txt labels
         save_json=False,  # save a COCO-JSON results file
-        project=ROOT / 'runs/val',  # save to project/name
+        project='runs/val',  # save to project/name
         name='exp',  # save to project/name
         exist_ok=False,  # existing project/name ok, do not increment
         half=True,  # use FP16 half-precision inference
