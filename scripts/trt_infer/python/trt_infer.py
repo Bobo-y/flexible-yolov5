4417ec219248c4c53500808835c7ccfbb72a93cf
Bobo~ <yanglinself@gmail.com>
2022-07-08 18:41:02 +0800

    Create trt_infer.py

---
+++ scripts/trt_infer/python/trt_infer.py
@@ -0,0 +1,312 @@
+"""
+An example that uses TensorRT's Python api to make inferences.
+"""
+import time
+import cv2
+import numpy as np
+import pycuda.autoinit
+import pycuda.driver as cuda
+import tensorrt as trt
+import torch
+import torchvision
+
+
+class YoLov5TRT():
+    """
+    description: A YOLOv5 class that warps TensorRT ops, preprocess and postprocess ops.
+    """
+
+    def __init__(self, engine_file_path):
+        # Create a Context on this device,
+        self.ctx = cuda.Device(0).make_context()
+        stream = cuda.Stream()
+        TRT_LOGGER = trt.Logger(trt.Logger.INFO)
+        runtime = trt.Runtime(TRT_LOGGER)
+
+        # Deserialize the engine from file
+        with open(engine_file_path, "rb") as f:
+            engine = runtime.deserialize_cuda_engine(f.read())
+        context = engine.create_execution_context()
+
+        host_inputs = []
+        cuda_inputs = []
+        host_outputs = []
+        cuda_outputs = []
+        bindings = []
+
+        for binding in engine:
+            print('bingding:', binding, engine.get_binding_shape(binding))
+            size = trt.volume(engine.get_binding_shape(
+                binding)) * engine.max_batch_size
+            dtype = trt.nptype(engine.get_binding_dtype(binding))
+            # Allocate host and device buffers
+            host_mem = cuda.pagelocked_empty(size, dtype)
+            cuda_mem = cuda.mem_alloc(host_mem.nbytes)
+            # Append the device buffer to device bindings.
+            bindings.append(int(cuda_mem))
+            # Append to the appropriate list.
+            if engine.binding_is_input(binding):
+                self.input_w = engine.get_binding_shape(binding)[-1]
+                self.input_h = engine.get_binding_shape(binding)[-2]
+                host_inputs.append(host_mem)
+                cuda_inputs.append(cuda_mem)
+            else:
+                host_outputs.append(host_mem)
+                cuda_outputs.append(cuda_mem)
+
+        # Store
+        self.stream = stream
+        self.context = context
+        self.engine = engine
+        self.host_inputs = host_inputs
+        self.cuda_inputs = cuda_inputs
+        self.host_outputs = host_outputs
+        self.cuda_outputs = cuda_outputs
+        self.bindings = bindings
+        self.batch_size = engine.max_batch_size
+
+    def infer(self, raw_image):
+        self.ctx.push()
+        # Restore
+        stream = self.stream
+        context = self.context
+        host_inputs = self.host_inputs
+        cuda_inputs = self.cuda_inputs
+        host_outputs = self.host_outputs
+        cuda_outputs = self.cuda_outputs
+        bindings = self.bindings
+        # Do image preprocess
+        ori_shape = raw_image.shape
+        batch_input_image = np.empty(
+            shape=[self.batch_size, 3, self.input_h, self.input_w])
+        input_image = self.preprocess_one(raw_image)
+        np.copyto(batch_input_image, input_image)
+        batch_input_image = np.ascontiguousarray(batch_input_image)
+
+        # Copy input image to host buffer
+        np.copyto(host_inputs[0], batch_input_image.ravel())
+        start = time.time()
+        # Transfer input data  to the GPU.
+        cuda.memcpy_htod_async(cuda_inputs[0], host_inputs[0], stream)
+        # Run inference.
+        context.execute_async(batch_size=self.batch_size,
+                              bindings=bindings, stream_handle=stream.handle)
+        # Transfer predictions back from the GPU.
+        cuda.memcpy_dtoh_async(host_outputs[0], cuda_outputs[0], stream)
+
+        # Synchronize the stream
+        stream.synchronize()
+        # Remove any context from the top of the context stack, deactivating it.
+        self.ctx.pop()
+        # Here we use the first row of output in that batch_size = 1
+        output = host_outputs
+        # Do postprocess
+        bboxes = self.post_process(output[0].reshape(1, 25200, 85), ori_shape)
+        return bboxes
+
+    def destroy(self):
+        self.ctx.pop()
+
+    def get_image(self, image_path):
+        return cv2.imread(image_path)
+
+    def scale_coords(self, img1_shape, coords, img0_shape, ratio_pad=None):
+        if ratio_pad is None:
+            # gain  = old / new
+            gain = min(img1_shape[0] / img0_shape[0],
+                       img1_shape[1] / img0_shape[1])
+            pad = (img1_shape[1] - img0_shape[1] * gain) / \
+                2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+        else:
+            gain = ratio_pad[0][0]
+            pad = ratio_pad[1]
+        coords[:, [0, 2]] -= pad[0]
+        coords[:, [1, 3]] -= pad[1]
+        coords[:, :4] /= gain
+        self.clip_coords(coords, img0_shape)
+        return coords
+
+    def clip_coords(self, boxes, img_shape):
+        boxes[:, 0].clamp_(0, img_shape[1])
+        boxes[:, 1].clamp_(0, img_shape[0])
+        boxes[:, 2].clamp_(0, img_shape[1])
+        boxes[:, 3].clamp_(0, img_shape[0])
+
+    def letterbox(self, img, new_shape=(640, 640), color=(114, 114, 114), auto=False, scaleFill=False, scaleup=True):
+        shape = img.shape[:2]
+        if isinstance(new_shape, int):
+            new_shape = (new_shape, new_shape)
+        r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+        if not scaleup:
+            r = min(r, 1.0)
+        ratio = r, r
+        new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+        dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
+        if auto:
+            dw, dh = np.mod(dw, 64), np.mod(dh, 64)
+        elif scaleFill:
+            dw, dh = 0.0, 0.0
+            new_unpad = (new_shape[1], new_shape[0])
+            ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]
+        dw /= 2
+        dh /= 2
+        if shape[::-1] != new_unpad:
+            img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+        top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+        left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+        img = cv2.copyMakeBorder(
+            img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+        return img, ratio, (dw, dh)
+
+    def preprocess_one(self, image):
+        img = self.letterbox(image, new_shape=640)[0]
+        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img = np.ascontiguousarray(img)
+        img = torch.from_numpy(img)
+        img = img.float()
+        img /= 255.0
+        img = np.expand_dims(img, 0)
+        return img
+
+    def xywh2xyxy(self, x):
+        y = torch.zeros_like(x) if isinstance(
+            x, torch.Tensor) else np.zeros_like(x)
+        y[:, 0] = x[:, 0] - x[:, 2] / 2
+        y[:, 1] = x[:, 1] - x[:, 3] / 2
+        y[:, 2] = x[:, 0] + x[:, 2] / 2
+        y[:, 3] = x[:, 1] + x[:, 3] / 2
+        return y
+
+    def non_max_suppression(self, prediction,
+                            conf_thres=0.25,
+                            iou_thres=0.45,
+                            classes=None,
+                            agnostic=False,
+                            multi_label=False,
+                            labels=(),
+                            max_det=300):
+
+        prediction = torch.from_numpy(prediction.astype(np.float32))
+        bs = prediction.shape[0]  # batch size
+        nc = prediction.shape[2] - 5  # number of classes
+        xc = prediction[..., 4] > conf_thres  # candidates
+
+        # Checks
+        assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
+        assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
+
+        # Settings
+        # min_wh = 2  # (pixels) minimum box width and height
+        max_wh = 7680  # (pixels) maximum box width and height
+        max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+        time_limit = 0.3 + 0.03 * bs  # seconds to quit after
+        redundant = True  # require redundant detections
+        multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+        merge = False  # use merge-NMS
+
+        t = time.time()
+        output = [torch.zeros((0, 6), device=prediction.device)] * bs
+        for xi, x in enumerate(prediction):  # image index, image inference
+            # Apply constraints
+            # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+            x = x[xc[xi]]  # confidence
+
+            # Cat apriori labels if autolabelling
+            if labels and len(labels[xi]):
+                lb = labels[xi]
+                v = torch.zeros((len(lb), nc + 5), device=x.device)
+                v[:, :4] = lb[:, 1:5]  # box
+                v[:, 4] = 1.0  # conf
+                v[range(len(lb)), lb[:, 0].long() + 5] = 1.0  # cls
+                x = torch.cat((x, v), 0)
+
+            # If none remain process next image
+            if not x.shape[0]:
+                continue
+
+            # Compute conf
+            x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+            # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+            box = self.xywh2xyxy(x[:, :4])
+
+            # Detections matrix nx6 (xyxy, conf, cls)
+            if multi_label:
+                i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+                x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+            else:  # best class only
+                conf, j = x[:, 5:].max(1, keepdim=True)
+                x = torch.cat((box, conf, j.float()), 1)[
+                    conf.view(-1) > conf_thres]
+
+            # Filter by class
+            if classes is not None:
+                x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+            # Apply finite constraint
+            # if not torch.isfinite(x).all():
+            #     x = x[torch.isfinite(x).all(1)]
+
+            # Check shape
+            n = x.shape[0]  # number of boxes
+            if not n:  # no boxes
+                continue
+            elif n > max_nms:  # excess boxes
+                # sort by confidence
+                x = x[x[:, 4].argsort(descending=True)[:max_nms]]
+
+            # Batched NMS
+            c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+            # boxes (offset by class), scores
+            boxes, scores = x[:, :4] + c, x[:, 4]
+            i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+            if i.shape[0] > max_det:  # limit detections
+                i = i[:max_det]
+            if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+                # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+                iou = self.box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+                weights = iou * scores[None]  # box weights
+                x[i, :4] = torch.mm(weights, x[:, :4]).float(
+                ) / weights.sum(1, keepdim=True)  # merged boxes
+                if redundant:
+                    i = i[iou.sum(1) > 1]  # require redundancy
+
+            output[xi] = x[i]
+            if (time.time() - t) > time_limit:
+                break  # time limit exceeded
+
+        return output
+
+    def post_process(self, outputs, ori_shape):
+        boxes = self.non_max_suppression(outputs)
+        re_boxes = []
+        for i, det in enumerate(boxes):
+            if det is not None and len(det):
+                det[:, :4] = self.scale_coords(
+                    (640, 640), det[:, :4], ori_shape).round()
+                for *xyxy, conf, cls in det:
+                    x_min = int(xyxy[0].cpu())
+                    y_min = int(xyxy[1].cpu())
+                    x_max = int(xyxy[2].cpu())
+                    y_max = int(xyxy[3].cpu())
+                    re_boxes.append(
+                        [x_min, y_min, x_max, y_max, conf.cpu(), cls.cpu()])
+
+        return re_boxes
+
+
+if __name__ == "__main__":
+
+    engine_file_path = "/home/yanglin5/PyProjects/yolov5_trt/model.trt"
+    yolov5_wrapper = YoLov5TRT(engine_file_path)
+    img_path = '/home/yanglin5/PyProjects/flexible-yolov5-2/test_imgs/cat_dog.jpg'
+    img = yolov5_wrapper.get_image(img_path)
+    try:
+        bboxes = yolov5_wrapper.infer(img)
+        print(bboxes)
+        for box in bboxes:
+            cv2.rectangle(img, (box[0], box[1]),
+                          (box[2], box[3]), (255, 0, 255), 2, 2)
+        cv2.imwrite('result.jpg', img)
+    finally:
+        yolov5_wrapper.destroy()
